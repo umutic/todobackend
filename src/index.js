d8c66d2cce5cae87eba54e52870f1d1cbc839dee
Umut Topkaya <umut@Umut-MacBook-Air.local>
2025-11-15 15:59:41 +0300

    vercel commit edildi

--- src/index.js
+++ src/index.js
@@ -27,6 +27,4 @@ app.use("/products", productRoutes);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log("Server started on port " + PORT);
-});
+export default app;
